81ba6715f76343b0e5e48d1c990e548c77e5206b
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2025-11-26 23:21:47 +0100

    GitPilot: Create medical_data/__init__.py - Create Package Structure with __init__.py Files

---
+++ medical_data/__init__.py
@@ -0,0 +1,326 @@
+__DementiaBank__
+ 
+The English Pitt corpus from DementiaBank, a shared database developed in the MacWhinney lab at Carnegie Mellon University. This database includes spontaneous speech (re-telling of the Cinderella story) from 167 subjects diagnosed with probable or possible Alzheimer's disease and 97 healthy control subjects. All subjects were recorded as part of the multi-site multi-year Pittsburgh Alzheimer Research Center (PARC) with funded support from NIH-NIA to Charles F. Reynolds III and Judith Saxton, at the University of Pittsburgh School of Medicine. This database uses the CHAT transcription format and the CLAN programs that accompany CHILDES.
+
+Download: https://dementia.talkbank.org/access/English/Pitt.html
+
+Clinical Research: https://dementia.talkbank.org/access/
+
+Project website: https://dementia.talkbank.org/
+
+Paper (DementiaBank): https://www.researchgate.net/publication/275714093_DementiaBank_Theoretical_Rationale_Protocol_and_Illustrative_Analyses
+
+*** 
+
+__SLI speech recordings__
+
+The Speechome Corpus is a database of audio and video recordings collected by Dr. Deb Roy's group at MIT of a two-year period in his son's life.  This database includes spontaneous speech that covers the whole two year span. The corpus, which may only be used for non-commercial purposes, is available upon request. 
+
+Paper and Access: https://dspace.mit.edu/handle/1721.1/52397
+
+***
+
+## 8. Medical Question/Answer Data
+
+__MedQuAD__
+
+A medical question-answering data set derived from 12 NIH websites (e.g. cancer.gov, niddk.nih.gov, GARD, MedlinePlus Health Topics).  It contains 47,457 medical question-answer pairs. 
+
+Paper: https://arxiv.org/abs/1901.08079
+
+Data: https://github.com/abachaa/MedQuAD
+
+
+__HealthTap__
+
+Medical question-answer pairs on HealthTap. Data are formatted as questions, answers, and the number of doctor agreements with the answer. 
+
+Data: https://github.com/durakkerem/Medical-Question-Answer-Datasets  (direct download: https://drive.google.com/file/d/1lyfqIwlLSClpaddle2b64RvgMJ-QwNMz8aGN3/view
+
+__HealthQA__
+
+Consumer medical question-answer pairs. Questions were crawled from Yahoo! Answers. The data set contains 1,134 training pairs and 260 test pairs.
+
+Data: https://github.com/durakkerem/Medical-Question-Answer-Datasets (direct download: https://drive.google.com/file/d/1lyfqIwlLSClpaddle2b64RvgMJ-QwNMz8aGN3/view)
+
+__iCliniq__ 
+
+Medical question-answer pairs on iCliniq. 
+
+Data: https://github.com/durakkerem/Medical-Question-Answer-Datasets (direct download: https://drive.google.com/file/d/1ZKjetk1YTy7wNOCqbNvh3BbeVrYnWvfX/view)
+
+__webmd__
+
+Medical question-answer pairs from WebMD.
+
+Data: https://github.com/durakkerem/Medical-Question-Answer-Datasets (direct download: https://drive.google.com/file/d/1RtJ15u-JE1xJnVB90spKCDL4KRnx16iW/view)
+
+***
+
+__MedInfo__
+
+A set of curated, commonly asked, patient queries from online medical resources. The dataset contains 4864 primary care patient queries. Additionally, it contains 4000 paraphrased queries to aid research in question similarity.
+
+Paper: https://www.aclweb.org/anthology/W19-5027/
+
+Data: https://github.com/clips/MedInfo
+
+***
+
+__emrQA__
+
+A large-scale dataset (~1M question-answer pairs) for question answering on electronic medical records. 
+
+Paper: https://arxiv.org/abs/1809.00732
+
+Data: https://github.com/panushri25/emrQA
+
+***
+
+__LivQA__
+
+A set of 200 consumer health questions that were collected by LivePerson and annotated by medical experts.
+
+Data: https://github.com/abachaa/LiveQA_MedicalTask_TREC2017
+
+## 9. Genetics Data
+
+__Cancer Gene Census__  
+Catalogue of genes associated with cancer from the Wellcome Sanger Institute  
+Access: http://cancer.sanger.ac.uk/census 
+
+***
+__ClinVar__  
+Public archive hosted by NCBI of reports of relationships among variations and phenotypes with supporting evidence  
+Access: http://www.ncbi.nlm.nih.gov/clinvar/ 
+
+***
+
+__Genotype-Tissue Expression (GTEx) project__  
+Searchable database with RNA-Seq gene expression in different tissues and individuals from dbGaP.  
+Information: http://www.gtexportal.org/home/  
+Access (requires dbGaP authentication): http://www.gtexportal.org/home/datasets  
+
+***
+__Human Gene Mutation Database__  
+Database of published human gene mutations. Requires registration. Data can only be used for direct research.  
+Information: http://www.hgmd.org  
+Access: http://www.hgmd.cf.ac.uk/ac/index.php  
+
+***
+__NHGRI Catalog of published genome-wide association studies__  
+Searchable database of all published GWAS studies that attempt to assay at least 100K SNPs in the initial stage.  
+Access: http://www.genome.gov/gwastudies/  
+
+***
+__The Cancer Genome Atlas__  
+Numerous data types on ~11K patients across 33 cancer types from NCI.  Data includes gene expression, SNPs, CNV, DNA methylation, miRNA, and protein expression.  __Requires registration__.   
+Information: http://cancergenome.nih.gov/  
+Access: https://tcga-data.nci.nih.gov/tcga/tcgaDownload.jsp  
+
+***
+
+__The Sequence Read Archive (SRA)__  
+
+An archive of raw sequence data from "the next generation" of sequencing platforms from NCBI. 
+
+Information: https://www.ncbi.nlm.nih.gov/sra/docs/
+
+Access: https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi
+
+__Gene Expression Omnibus (GEO)__  
+
+A public functional genomics archive repository of microarray, next-generation sequencing, and other forms of high-throughput functional genomics data.
+
+Information: https://www.ncbi.nlm.nih.gov/geo/info/overview.html
+
+Access: https://www.ncbi.nlm.nih.gov/gds
+
+***
+
+## 10. Human Mobility Data
+
+__SafeGraph__
+
+SafeGraph has made their mobility data available for free for COVID-19-related research. It contains foot-traffic (movement and aggregation) patterns of Points Of Interests (e.g. restaurants, retailers, churches, etc.) in the US.
+
+Paper: None.
+
+Application form: https://www.safegraph.com/academics
+
+Access: https://docs.safegraph.com/docs
+
+***
+
+__Google Community Mobility Reports__
+
+Google reports on aggregated mobility trends from their location data over time by geography across different place categories (retail and recreation, grocery and pharmacy, parks, transit, workplaces, and residential). 
+
+Paper: None
+
+Access: https://www.google.com/covid19/mobility/
+
+
+__Apple Maps Mobility Trends Report__
+
+Apple reports a relative volume of Directions requests on Apple Maps compared to baseline volume on January 13th, 2020. Data are reported daily for different regions (countries/regions and cities) and different transportation types (driving, walking, or transit). 
+
+Paper: None
+
+Access: https://www.apple.com/covid19/mobility
+
+
+__Facebook Symptom Survey__
+
+Facebook and CMU Delphi Center have been running a daily survey asking Facebook users about COVID-19 symptoms. The website can provide access to individual response data.
+
+Paper: https://arxiv.org/abs/2009.14675
+
+Access: https://dataforgood.fb.com/docs/covid-19-symptom-survey-request-for-data-access/
+
+More information: https://delphi.cmu.edu/blog/2020/08/26/covid-19-symptom-surveys-through-facebook/
+
+
+__Facebook Social Connectedness Index__
+
+The Social Connectedness Index (SCI) is a measure of the intensity of the connectedness between two places. It measures the relative frequency of Facebook friendship between every pair of countries/regions (administrative units).
+
+Paper: None
+
+Access: https://dataforgood.fb.com/tools/social-connectedness-index/
+
+More information: https://research.fb.com/publications/the-economic-effects-of-social-networks-evidence-from-the-housing-market/
+
+
+__Facebook Movement Range Maps__ 
+
+Facebook Movement Range Maps show how populations are moving and how the COVID-19 crisis is affecting those movements. Facebook creates tiles with data aggregated from those Facebook users who have Location History turned on. 
+
+Paper: https://research.fb.com/wp-content/uploads/2020/06/COVID-19-Mobility-Data-Aggregation-Privacy-Report.pdf
+
+Access: https://dataforgood.fb.com/tools/movement-range-maps/
+
+
+## 11. Public Health Data
+
+__US COVID-19 Daily Cases, Deaths, and Testing from The COVID Tracking Project__
+
+COVID-19 cases, deaths, and testing data from 50 US states, 5 territories, and the District of Columbia. The data available through the project's website is aggregated from state/district/territory public health authorities (or, occasionally, from trusted news reporting, official press conferences, or social media updates from state public health authorities or governors). The project started collecting these data on March 7, 2020 and the data collection will retire on March 7, 2021.
+
+ 
+Website: https://covidtracking.com/
+
+API/Access: https://covidtracking.com/data/api
+
+***
+
+__COVID-19 Hospitalizations from HealthData.gov__
+
+COVID-19 reported patient impact and hospital capacity data, aggregated from approximately 6,000 hospitals that have been identified by the federal government as acute care hospitals. This includes all hospitals registered with CMS as acute care hospitals, children's hospitals, cancer hospitals, or critical access hospitals. The dataset includes aggregated estimates of COVID patients currently in inpatient beds and ICUs for all 50 US states, 5 territories, and the District of Columbia at state-level and county-level. It also includes hospital bed and ICU capacity for each state.
+
+Access: https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-state-timeseries
+
+
+***
+
+__County-level COVID-19 Deaths from USAFacts__
+
+County-level COVID-19 confirmed deaths data for the US. USAFacts receives data from federal, state, and local governments, using credible data sources such as government websites, government social media accounts, direct e-mail, and telephone correspondences. For COVID-19-specific information, USAFacts follows the data collection and reporting practices of the Centers for Disease Control and Prevention (CDC). 
+
+
+Access: https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/
+
+***
+
+
+__County-level COVID-19 Cases and Deaths from The New York Times__
+
+County-level COVID-19 confirmed cases and deaths data for the US. The New York Times compiles this time series data from state and local governments and health departments in an attempt to provide a complete record of the ongoing outbreak. 
+
+
+Access: https://github.com/nytimes/covid-19-data
+
+***
+
+__US Hospital Locations from Definitive Healthcare__
+
+A list of US hospital locations, number of beds, type of hospital, hospital owner, and other descriptive information of hospital facilities. The data were collected by Definitive Healthcare from the CMS National Provider Identifier Data and directly from the hospital facilities. The data are contributed by Esri to inform healthcare and emergency response during the COVID-19 crisis.
+
+
+Access: https://coronavirus-resources.esri.com/datasets/definitivehc::definitive-healthcare-usa-hospital-beds
+
+*** 
+
+__County-level Social Vulnerability Index from CDC/ATSDR__
+
+CDC Social Vulnerability Index (SVI) uses US census variables to help local officials identify communities that may need support before, during, or after disasters. The SVI ranks the social vulnerability (4 related themes: socioeconomic status, household composition and disability, minority status and language, and housing type and transportation) of every census tract in the US. In addition to the map and data files are a supplemental document that describes the rationale, methods, and limitations to the SVI. 
+
+Access: https://svi.cdc.gov/index.html
+
+*** 
+
+__County-level Health Context and Outcomes from County Health Rankings and Roadmaps__
+
+The CHR&R provides annual rankings of the health of nearly every county in the nation. CHR&R provides a snapshot of a community's health by creating rankings of health outcomes and health factors. This includes social determinants and environmental conditions.  
+
+Access: https://www.countyhealthrankings.org/explore-health-rankings/rankings-data-documentation
+
+*** 
+
+__COVID-19-related Legislations from State Legislatures__
+
+An aggregated list of legislation, bills, and resolutions related to COVID-19 introduced in state legislatures across the US. The National Conference of State Legislatures tracks actions states are taking to respond to the coronavirus (COVID-19) pandemic and provides weekly updates on relevant state legislation. 
+
+Access: https://www.ncsl.org/research/health/state-action-on-coronavirus-covid-19.aspx
+
+***
+
+__COVID-19-related State and Territorial Policies from the National Governors Association__
+
+An aggregated list of COVID-19 policy recommendations from state and territorial governors. The NGA collects these policy recommendations from the NGA/government office, the public site of the governor's office, or media outlets. Currently available actions include stay-at-home orders, extended school closures, emergency declarations, and the National Guard activations. 
+
+Access: https://www.nga.org/coronavirus/#actions
+
+***
+
+__COVID-19-related State Orders from the National Academy for State Health Policy (NASHP)__
+
+A chart presenting stay-at-home orders, mask requirements, and re-opening status across US states. States have taken different actions to curb the spread of the virus, which have evolved over the course of the pandemic. The chart is updated periodically based on an independent search of current executive orders or policy statements from governors' offices. 
+
+Access: https://www.nashp.org/governors-prioritize-health-for-all/
+
+***
+
+__Global COVID-19 Data from Johns Hopkins Center for Systems Science and Engineering__
+
+Global COVID-19 aggregated cases and deaths on the country/regional level. The data are primarily sourced from the reports of the World Health Organization (WHO), the US Centers for Disease Control and Prevention (US CDC), the China CDC (CCDC), the European Center for Disease Prevention and Control (ECDC), and the NHC of the People's Republic of China. US state- and county-level data are sourced from state and local health departments.  
+
+
+Website: https://coronavirus.jhu.edu/map.html
+
+Access: https://github.com/CSSEGISandData/COVID-19
+
+***
+
+__Global COVID-19 Case Counts from WHO__
+
+WHO COVID-19 cases and deaths data. The data is aggregated from the WHO regional offices. WHO offices periodically receive data from the officially-designated national focal points for the International Health Regulations (IHR).  
+
+Access: https://covid19.who.int/
+
+***
+
+__US County-level Social Distancing Metrics from Unacast__
+
+Unacast's U.S. Social Distancing Dataset includes daily measures of social distancing activities in every county (and state) in the U.S. It measures change in visitation to non-essential venues and businesses, change in distance traveled, and change in (population) encounters, all of which serve as proxies for social distancing. The data are aggregated from GPS location data from 15-17 million anonymous mobile phones per day (Unacast data partners). 
+
+Access: https://www.unacast.com/data-for-good (requires registration)
+
+***
+
+## 12. Bio-Signals Data
+
+
+__PTB Diagnostic ECG Database__
+
+One of the datasets used in the Physionet/Computing in Cardiology Challenge 2017. Contains  549 records from 290 subjects (aged 17 to 87, mean 57.2; 209 men, mean age 55.5, and 81 women, mean age 61.6; ages were not recorded for 1 female and 14 male subjects). Each subject is represented by one to five records. There are ECG recordings from healthy volunteers and patients with different heart diseases. Each recor
